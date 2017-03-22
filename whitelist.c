ed999470c1818d13b823cd59ca356bc9beff9f9c
ZZIQIN <ziqin@outlook.com>
2017-03-22 20:35:50 +0800

    Update whitelist.c

--- whitelist.c
+++ whitelist.c
@@ -45,8 +45,8 @@ uint32_t wl_num_tag = (1 << (sizeof(uint32_t) * 8 - WL_TAG_BITS)) | 0;
 time_t last_modification = 0;
 struct redisWhitelist redis_w_list;	// connect白名单
 
-pthread_mutex_t mlock = PTHREAD_MUTEX_INITIALIZER;
-pthread_t thread_id;    //白名单更新线程
+static pthread_mutex_t mlock = PTHREAD_MUTEX_INITIALIZER;
+static pthread_t thread_id;    //白名单更新线程
 int initialized=0;
 
 struct wl_config config;
